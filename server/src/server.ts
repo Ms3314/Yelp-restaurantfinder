da493704afd83cc553a75b1099f3008379d1b006
Samiuddin <mdsamiuddin2005@outlook.com>
2024-12-22 11:52:39 +0530

    the backend is almost complete here part 2:47:38

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,10 @@
 require("dotenv").config();
 import express from 'express';
 import morgan from 'morgan';
-import pg from 'pg'
 import { Client } from 'pg'; 
 
-
-
-// const db = new Client({
-//     user: process.env.PGUSER,
-//     password: process.env.PGPASSWORD,
-//     host: process.env.PGHOST,
-//     port: parseInt(process.env.PGPORT || "5432"), // Provide a default value for PGPORT
-//     database: process.env.PGDATABASE,
-// })
 const db = new Client()
  
-
-
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
@@ -24,7 +12,6 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 // database connected 
-
 db.connect().then(() => {
     console.log('Database connected successfully.');
   }).catch((err) => {
@@ -50,17 +37,25 @@ app.get('/api/v1/restaurants' , async (req , res) => {
 })
 
 //adding the restaurant data 
-app.post("/api/v1/restaurants" , (req ,res) =>{
-    console.log(req.body);
-    res.status(201).json({
-        status : "success" ,
-        data: {
-            restaurant : ['mcdonals','Cafe555','Karachi bakkery','Pista House']
-        }
-    })
+app.post("/api/v1/restaurants" ,async (req ,res) =>{
+    try {
+        let {name , location , price_range} = req.body;
+        const results = await db.query("INSERT INTO restaurants (name , location , price_range) VALUES ($1 , $2 , $3) returning *",[name , location , price_range ])
+        res.status(201).json({
+            status : "success" ,
+            message : "data addded succesfully",
+            data : results.rows
+        })
+    } catch (error) {
+        res.status(500).json({
+            err : error
+        })
+    }
+    
 
 })
 
+// getting a specific restaurant with using an ID 
 app.get('/api/v1/restaurants/:id' ,async (req , res)=>{
     try {
         const resid = req.params.id ;
@@ -79,23 +74,43 @@ app.get('/api/v1/restaurants/:id' ,async (req , res)=>{
     
 })
 
-app.put("/api/v1/restaurants/:id" , (req , res) =>{
-    console.log(req.params.id)
-    console.log(req.body)
-    res.status(200).json({
-        status : "success" ,
-        data: {
-            restaurant : ['mcdonals','Cafe555','Karachi bakkery','Pista House']
-        }
-    })
+// Update a particular restaurant Data
+app.put("/api/v1/restaurants/:id" , async (req , res) =>{
+    try {
+        const id = req.params.id ;
+        let {name , location , price_range} = req.body;
+        const results = await  db.query("UPDATE restaurants SET name= $2 , location= $3 , price_range= $4 WHERE id = $1 returning *",[id , name , location , price_range ]);
+        console.log(req.params.id);
+        console.log(req.body);
+        console.log(results , "these are the results");
+        res.status(200).json({
+            status : "success" ,
+            data: { 
+                restaurant : results.rows
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            err : error
+        })
+    }
+
+    
 })
+
 // Deleting a restaurant 
-app.delete("/api/v1/restaurants/:id" , (req , res) =>{
-    console.log(req.params.id)
-    console.log(req.body)
-    res.status(200).json({
-        status : "success" ,
-    })
+app.delete("/api/v1/restaurants/:id" ,async (req , res) =>{
+    try {
+        const id = req.params.id;
+        await db.query("DELETE FROM restaurants WHERE id = $1" , [id]);
+        res.status(200).json({
+            status : "success" ,
+        })
+    } catch (error) {
+        res.status(500).json({
+            err : error
+        })
+    }
 })
 
 app.listen(PORT , ()=>{
